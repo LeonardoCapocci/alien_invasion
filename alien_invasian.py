22c954f8c54e755c41cdbd4af3eaed8e6f644ddf
LeonardoCapocci <leocap2001@gmail.com>
2023-08-30 10:55:48 -0700

    created bullets that shoot from ship

--- alien_invasian.py
+++ alien_invasian.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasian:
     """Overall class to manage game assets and behavior."""
@@ -13,17 +14,21 @@ class AlienInvasian:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
         
-        self.screen = pygame.display.set_mode(
-            (0,0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        #self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        #self.settings.screen_width = self.screen.get_rect().width
+        #self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion - Leonardo Capocci")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
             
             self.clock.tick(60)
@@ -46,16 +51,25 @@ class AlienInvasian:
             self.ship.moving_right = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
     
     def _check_keyup_events(self, event):
         if event.key == pygame.K_LEFT:
             self.ship.moving_left = False
         elif event.key == pygame.K_RIGHT:
             self.ship.moving_right = False 
+    
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_screen(self):
-        """Update images on the screen, and flip to the new screen"""
+        """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
 
         pygame.display.flip()
