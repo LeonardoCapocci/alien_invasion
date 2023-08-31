ca902a450461d0ec85a8ab616bcbe6a0e0e16131
LeonardoCapocci <leocap2001@gmail.com>
2023-08-31 12:19:32 -0700

    full row of aliens

--- alien.py
+++ alien.py
@@ -14,7 +14,7 @@ class Alien(Sprite):
 
         # Start each new alien near the top left of the screen
         self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
+        self.rect.y = self.rect.height * 0.2
 
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
--- alien_invasian.py
+++ alien_invasian.py
@@ -82,8 +82,18 @@ class AlienInvasian:
     
     def _create_fleet(self):
         """Create the fleet of aliens."""
-        # Make an alien.
+        # Create an alien and keep adding alients until there's on room left.
+        # Spacing between aliens is one alien width
         alien = Alien(self)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += alien_width * 2
         self.aliens.add(alien)
 
     def _update_screen(self):
