ff989370cac2cc1c99c137388dfb13996c4d6980
LeonardoCapocci <leocap2001@gmail.com>
2023-09-06 16:00:25 -0700

    aliens can be shot down

--- alien.py
+++ alien.py
@@ -7,6 +7,7 @@ class Alien(Sprite):
         """Initialize alien attributes and set starting position"""
         super().__init__()
         self.screen = game.screen
+        self.settings = game.settings
 
         # Load the alien image
         self.image = pygame.image.load('images/alien.bmp')
@@ -18,5 +19,15 @@ class Alien(Sprite):
 
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
+    
+    def check_edges(self):
+        """Return True if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+
+    def update(self):
+        """Move the aliens to the right or left."""
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.rect.x = self.x
 
         
